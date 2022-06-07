a3856ac38ed7bdce2f19c3287f9143f58dbee173
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-06-07 19:08:43 +0200

    Progression

--- Consolidation_4-Hangman/ViewController.swift
+++ Consolidation_4-Hangman/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     var livesLeft: UILabel!
     var currentWord: UILabel!
     var guessLetter: UITextField!
-    var wordsList = [String]()
+    var clueForWord: UILabel!
 
     override func viewDidLoad() {
         view = UIView()
@@ -35,6 +35,13 @@ class ViewController: UIViewController {
         guessLetter.placeholder = "Tap to type in a letter."
         view.addSubview(guessLetter)
         
+        clueForWord  = UILabel()
+        clueForWord.translatesAutoresizingMaskIntoConstraints = false
+        clueForWord.font = UIFont.systemFont(ofSize: 44)
+        clueForWord.textAlignment = .center
+        clueForWord.text = "A clue"
+        view.addSubview(clueForWord)
+        
         NSLayoutConstraint.activate ([
             livesLeft.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
             livesLeft.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
@@ -43,14 +50,62 @@ class ViewController: UIViewController {
             
             currentWord.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentWord.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 10),
-            currentWord.bottomAnchor.constraint(equalTo: guessLetter.topAnchor),
+            currentWord.bottomAnchor.constraint(equalTo: clueForWord.topAnchor),
             currentWord.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75),
             currentWord.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.75),
             
-            guessLetter.topAnchor.constraint(equalTo: currentWord.bottomAnchor),
             guessLetter.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            guessLetter.heightAnchor.constraint(equalToConstant: 40)
+            guessLetter.topAnchor.constraint(equalTo: currentWord.bottomAnchor),
+            guessLetter.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -30),
+            guessLetter.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75),
+            guessLetter.heightAnchor.constraint(equalToConstant: 40),
+            
+            clueForWord.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            clueForWord.topAnchor.constraint(equalTo: currentWord.topAnchor, constant: 30),
+            clueForWord.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 10),
+            clueForWord.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            clueForWord.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.03, constant: 30)
         ])
+        
+        //loadGame()
     }
+    
+    var wordsList = [String]()
+    var lives = 7 {
+        didSet {
+            livesLeft.text = "Lives: \(lives)"
+        }
+    }
+    
+    /*func loadGame() {
+        var clueString = ""
+        var wordString = ""
+        
+        if let gameFileURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let gameContent = try? String(contentsOf: gameFileURL) {
+                var lines = gameContent.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                for line in lines {
+                    let parts = line.components(separatedBy: ": ")
+                    let word = parts[0]
+                    let clue = parts[1]
+                    
+                    wordString += "\(word)"
+                    clueString += "\(clue)"
+                    
+                    //wordsList.append(wordString)
+                }
+            }
+        }
+        //clueForWord.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        //currentWord.text = wordString.trimmingCharacters(in: .whitespacesAndNewlines)
+    }*/
+    
+    /*func submitLetter(_ sender: UITextField) {
+        guard let currentLetter = guessLetter.text else { return }
+        
+        
+    }*/
     
 }
